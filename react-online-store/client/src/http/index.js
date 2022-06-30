8fdd61e51461f0640c34f471a369766b5e2e7722
Anton Bozhko <anton.bozhko04@gmail.com>
2022-06-30 22:52:32 +0300

    online-setore dev alpha(v0.55)

--- react-online-store/client/src/http/index.js
+++ react-online-store/client/src/http/index.js
@@ -1,6 +1,17 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: "",
+    baseURL: ProcessingInstruction.env.REACT_APP_API_URL,
 });
-const $authHost = axios.create({});
+const $authHost = axios.create({
+    baseURL: ProcessingInstruction.env.REACT_APP_API_URL,
+});
+
+const authInterceptor = (config) => {
+    config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+};
+
+$authHost.interceptors.request.use(authInterceptor);
+
+export { $host, $authHost };
